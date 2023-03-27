a705c28ee836f9d7387881e98b07f5d5cba6f0f2
perfectra1n <jonfuller2012@gmail.com>
2023-03-27 12:41:35 -0700

    try something like this?

---
+++ generate-trilium-client.fish
@@ -0,0 +1,32 @@
+#!/usr/bin/env fish
+# Define the URL of the YAML file
+set url https://github.com/zadam/trilium/blob/master/src/etapi/etapi.openapi.yaml
+
+# Download the YAML file using curl and save it as etapi.yaml
+curl -L $url -o etapi.yaml
+
+# Check if virtualenv is installed
+if not type -q virtualenv
+    # If not, install it using pip
+    pip install virtualenv
+end
+
+# Create a virtualenv called venv if it does not exist
+if not test -d venv
+    virtualenv venv
+end
+
+# Activate the virtualenv
+source venv/bin/activate.fish
+
+# Check if openapi-python-client is installed in the virtualenv
+if not type -q openapi-python-client
+    # If not, install it using pip
+    pip install openapi-python-client
+end
+
+# Use openapi-python-client to generate a Python client from the YAML file
+openapi-python-client generate --path etapi.yaml
+
+# Deactivate the virtualenv
+deactivate
