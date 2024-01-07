aa48a5bff7ff94d43135fb8737fe57496eede5e0
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-01-07 00:21:38 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/api/default/get_week_note.py
+++ etapi-client/etapi_client/api/default/get_week_note.py
@@ -7,9 +7,9 @@ from ...client import AuthenticatedClient, Client
 from ...types import Response
 from ... import errors
 
-import datetime
 from typing import Dict
 from ...models.note import Note
+import datetime
 
 
 def _get_kwargs(
