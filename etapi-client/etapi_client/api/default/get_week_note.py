8458e1ed9fc239ca40b19f9371c9f820596993b4
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-01-16 00:21:40 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/api/default/get_week_note.py
+++ etapi-client/etapi_client/api/default/get_week_note.py
@@ -7,8 +7,8 @@ from ...client import AuthenticatedClient, Client
 from ...types import Response
 from ... import errors
 
-from typing import Dict
 import datetime
+from typing import Dict
 from ...models.note import Note
 
 
