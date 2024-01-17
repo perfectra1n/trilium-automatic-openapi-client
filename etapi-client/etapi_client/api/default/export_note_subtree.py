bb691e6f59048192bd3ed84e9bd6d32dc77c916b
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-01-17 00:21:27 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/api/default/export_note_subtree.py
+++ etapi-client/etapi_client/api/default/export_note_subtree.py
@@ -8,8 +8,8 @@ from ...types import Response, UNSET
 from ... import errors
 
 from ...models.export_note_subtree_format import ExportNoteSubtreeFormat
-from ...types import UNSET, Unset
 from typing import Union
+from ...types import UNSET, Unset
 
 
 def _get_kwargs(
--- etapi-client/etapi_client/models/search_response.py
+++ etapi-client/etapi_client/models/search_response.py
@@ -8,10 +8,10 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
-from typing import List
-from ..types import UNSET, Unset
-from typing import Union
 from typing import Dict
+from typing import List
+from typing import Union
+from ..types import UNSET, Unset
 
 if TYPE_CHECKING:
     from ..models.search_response_debug_info import SearchResponseDebugInfo
