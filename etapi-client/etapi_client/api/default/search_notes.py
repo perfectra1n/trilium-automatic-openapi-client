0280e8966402f1634aa42768297ae0b6b956f61c
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-01-18 00:21:35 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/api/default/search_notes.py
+++ etapi-client/etapi_client/api/default/search_notes.py
@@ -7,11 +7,11 @@ from ...client import AuthenticatedClient, Client
 from ...types import Response, UNSET
 from ... import errors
 
+from ...types import UNSET, Unset
 from typing import Dict
+from typing import Union
 from ...models.search_notes_order_direction import SearchNotesOrderDirection
 from ...models.search_response import SearchResponse
-from typing import Union
-from ...types import UNSET, Unset
 
 
 def _get_kwargs(
--- etapi-client/etapi_client/models/note_with_branch.py
+++ etapi-client/etapi_client/models/note_with_branch.py
@@ -8,9 +8,9 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
+from ..types import UNSET, Unset
 from typing import Dict
 from typing import Union
-from ..types import UNSET, Unset
 
 if TYPE_CHECKING:
     from ..models.branch import Branch
