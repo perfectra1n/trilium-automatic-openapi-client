05588deb56d75ee0d376ac89fea893fe2e69ce1f
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-01-22 00:21:31 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/models/search_response.py
+++ etapi-client/etapi_client/models/search_response.py
@@ -8,14 +8,14 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
-from typing import List
-from typing import Union
 from ..types import UNSET, Unset
 from typing import Dict
+from typing import Union
+from typing import List
 
 if TYPE_CHECKING:
-    from ..models.search_response_debug_info import SearchResponseDebugInfo
     from ..models.note import Note
+    from ..models.search_response_debug_info import SearchResponseDebugInfo
 
 
 T = TypeVar("T", bound="SearchResponse")
@@ -58,8 +58,8 @@ class SearchResponse:
 
     @classmethod
     def from_dict(cls: Type[T], src_dict: Dict[str, Any]) -> T:
-        from ..models.search_response_debug_info import SearchResponseDebugInfo
         from ..models.note import Note
+        from ..models.search_response_debug_info import SearchResponseDebugInfo
 
         d = src_dict.copy()
         results = []
