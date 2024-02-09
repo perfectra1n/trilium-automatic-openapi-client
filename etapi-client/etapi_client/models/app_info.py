45fc51b292f1e602a2f89e83115d50d72d61fa6a
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-02-09 00:21:50 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/models/app_info.py
+++ etapi-client/etapi_client/models/app_info.py
@@ -8,9 +8,9 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
+import datetime
 from dateutil.parser import isoparse
 from typing import Union
-import datetime
 
 
 T = TypeVar("T", bound="AppInfo")
