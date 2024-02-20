f17d892192114c59b49e40a374373a0c13aca184
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-02-20 00:15:08 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/models/app_info.py
+++ etapi-client/etapi_client/models/app_info.py
@@ -8,8 +8,8 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
-from typing import Union
 from dateutil.parser import isoparse
+from typing import Union
 import datetime
 
 
