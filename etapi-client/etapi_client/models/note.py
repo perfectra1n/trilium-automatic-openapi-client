073b338c6b0927bb7441bd51d722e9bc84a59860
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-03-05 00:09:08 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/models/note.py
+++ etapi-client/etapi_client/models/note.py
@@ -8,8 +8,8 @@ from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
-from typing import Union
 from ..models.note_type import NoteType
+from typing import Union
 from typing import cast
 
 if TYPE_CHECKING:
