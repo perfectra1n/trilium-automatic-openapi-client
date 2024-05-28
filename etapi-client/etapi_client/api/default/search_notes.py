ebad0a4f9355d101115e4a3183802586d8520e1b
perf3ct <perf3ct@noreply-fullerhomenetwork@gmail.com>
2024-05-28 00:20:29 +0000

    [DRONE] [CI SKIP] Updated client

--- etapi-client/etapi_client/api/default/search_notes.py
+++ etapi-client/etapi_client/api/default/search_notes.py
@@ -7,9 +7,9 @@ from ...client import AuthenticatedClient, Client
 from ...types import Response, UNSET
 from ... import errors
 
+from ...models.search_response import SearchResponse
 from ...types import Unset
 from ...models.search_notes_order_direction import SearchNotesOrderDirection
-from ...models.search_response import SearchResponse
 
 
 def _get_kwargs(
